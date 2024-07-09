cf8a5500eadcab0d92995ea361817ceba93a8540
user <IsaevaAliya@github.com>
2024-07-09 13:13:18 +0600

    hw2_al

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class BankAccount {
+public class BankAccount {
+    private double amount;
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void deposit(double som) {
+        amount += som;
+    }
+
+    public void withDraw(int som) throws LimitException {
+        if (som > amount) {
+            throw new LimitException("Insufficient funds for withdrawal", amount);
+        }
+        amount -= som;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BankAccount account = new BankAccount();
+        account.deposit(15000);
+
+        while (true) {
+            try {
+                account.withDraw(6000);
+                System.out.println("Successfully withdrew 6000. Remaining balance: " + account.getAmount());
+            } catch (LimitException e) {
+                System.out.println(e.getMessage());
+                System.out.println("Withdrawing the remaining amount: " + e.getRemainingAmount());
+                try {
+                    account.withDraw((int) e.getRemainingAmount());
+                } catch (LimitException limitException) {
+                    // Этот блок не должен быть достигнут
+                    limitException.printStackTrace();
+                }
+                break;
+            }
+        }
     }
 }
